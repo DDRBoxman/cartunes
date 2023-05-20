0f5e1ba6b5b00020f0f494cbb35c5d36d86161f5
Colin Edwards <colin@recursivepenguin.com>
2023-05-19 21:25:38 -0500

    Initial Commit

---
+++ cartunes/cartunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cartunesApp.swift
+//  cartunes
+//
+//  Created by Colin Edwards on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct cartunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
